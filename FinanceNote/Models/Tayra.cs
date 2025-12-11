82a730d5980903db4ad4cb15f7e8431d0138a5f4
太良 桜井 <taira_sakurai@outlook.jp>
2025-12-11 17:25:06 +0900

    プロジェクトファイルを追加。

---
+++ FinanceNote/Models/Tayra.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceNote.Models
+{
+    [Table("Tayra")]
+    public class Tayra
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "日時")]
+        public DateTime Time { get; set; }
+
+        [Required]
+        [Display(Name = "内容")]
+        public string? Title { get; set; }
+
+        [Required]
+        [Display(Name = "現金")]
+        public decimal Cash { get; set; }
+
+        [Required]
+        [Display(Name = "ICOCA")]
+        public decimal Icoca { get; set; }
+
+        [Required]
+        [Display(Name = "生協電子マネー")]
+        public decimal Coop { get; set; }
+    }
+}
