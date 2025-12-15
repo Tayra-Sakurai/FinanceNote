e4bf41bbdcbcb0b4e2489fe53d7c30146477e9db
太良 桜井 <taira_sakurai@outlook.jp>
2025-12-15 21:50:02 +0900

    Added notifyscript.js.

---
+++ FinanceNote/wwwroot/js/notifyscript.js
@@ -0,0 +1,36 @@
+﻿/**
+ * @fileoverview This is the front end script.
+ * @author Tayra Sakurai <b4151069@edu.kit.ac.jp>
+ * @copyright Copyright (C) 2025 Tayra Sakurai
+ * @license Copyright (C) 2025 Tayra Sakurai
+ * 
+ * This is a part of FinanceNote.
+ * 
+ * FinanceNote is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * 
+ * FinanceNote is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with FinanceNote. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+async function sendNotificationAsync() {
+    try {
+        /**
+         * Registrate the service worker.
+         */
+        navigator.serviceWorker.register(
+            '/js/sw.js'
+        )
+            .then(
+                serviceWorkerRegistration => {
+                    serviceWorkerRegistration.pushManager.subscribe();
+                },
+                error => {
+                    alert(error);
+                }
+            );
+    } catch (e) {
+        alert('Failed to send message.');
+    }
+}
